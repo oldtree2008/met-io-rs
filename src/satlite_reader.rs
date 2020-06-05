6392ac6d6bb7579f57cfc6ce29af985b31d1b9d3
cheng <csc00000@sina.com>
2020-06-05 17:53:56 +0800

    use binread parse awx

--- src/satlite_reader.rs
+++ src/satlite_reader.rs
@@ -1,355 +1,135 @@
 #![allow(non_snake_case)]
 use super::MetError;
 use super::MetReader;
-use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
+
 use std::io::Cursor;
 use std::result::Result;
-use std::string::String;
+
+use binread::prelude::*;
+use binread::NullString;
+use std::io::SeekFrom;
+
+#[derive(Debug,BinRead)]
+#[br(little)]
+struct Product {    
+    fName:NullString,
+    #[br(seek_before = SeekFrom::Start(12))]
+    byteSequence:u16,
+    #[br(is_little=(byteSequence==0))]
+    firstClassHeadLength:i16,
+    #[br(is_little=(byteSequence==0))]
+    secondClassHeadLength:i16,
+    #[br(is_little=(byteSequence==0))]
+    padDataLength:i16,
+    #[br(is_little=(byteSequence==0))]
+    recordLength:i16,
+    #[br(is_little=(byteSequence==0))]
+    headRecordNumber:i16,
+    #[br(is_little=(byteSequence==0))]
+    dataRecordNumber:i16,
+    #[br(is_little=(byteSequence==0))]
+    productCategory:i16,
+    #[br(is_little=(byteSequence==0))]
+    compressMethod:i16,
+    formatString:NullString,
+    #[br(is_little=(byteSequence==0),seek_before = SeekFrom::Start(38))]
+    qualityFlag:i16,
+
+    //for product1
+    #[br(is_little=(byteSequence==0),if(productCategory==1))]
+    header1:Option<Header1>,
+    #[br(is_little=(byteSequence==0),if(productCategory==1),calc=header1.as_ref().unwrap().widthOfImage  as u64)]
+    data1_width:Option<u64>,
+    #[br(is_little=(byteSequence==0),if(productCategory==1),calc=header1.as_ref().unwrap().heightOfImage  as u64)]
+    data1_height:Option<u64>,
+    #[br(is_little=(byteSequence==0),if(productCategory==1),calc=data1_height.unwrap() * data1_width.unwrap())]
+    data1_len:Option<u64>,
+    #[br(is_little=(byteSequence==0),if(productCategory==1),calc=(headRecordNumber*recordLength) as u64)]
+    data1_start:Option<u64>,
+    #[br(is_little=(byteSequence==0),if(productCategory==1),seek_before=SeekFrom::Start(data1_start.unwrap()),count=data1_len.unwrap())]
+    data1:Option<Vec<u8>>,
+    
+    
+    //HeadExt
+    #[br(calc=(firstClassHeadLength +secondClassHeadLength + padDataLength) as u64)]
+    index:u64,
+    #[br(seek_before=SeekFrom::Start(index),pad_size_to=64)]
+    fileName:NullString,
+    #[br(pad_size_to=8)]
+    formateVersion:NullString,
+    #[br(pad_size_to=8)]
+    manufacturer:NullString,
+    #[br(pad_size_to=16)]
+    satelliteName:NullString,
+    #[br(pad_size_to=8)]
+    instrumentName:NullString,
+    #[br(pad_size_to=8)]
+    channelCount:NullString,
+    #[br(pad_size_to=8)]
+    programVersion:NullString,
+    #[br(pad_size_to=8)]
+    copyright:NullString,
+    #[br(pad_size_to=8)]
+    sizeFilled:NullString,
+}
+
+#[derive(Debug,BinRead)]
+#[br(little)]
+struct Header1 {
+    satelliteName: NullString,
+    #[br(seek_before = SeekFrom::Start(48))]
+    year:u16,
+    month:u16,
+    day:u16,
+    hour:u16,
+    minute:u16,
+    channel:i16,
+    flagOfProjection:i16,
+    widthOfImage:i16,
+    heightOfImage:i16,
+    scanLineNumberOfImageTopLeft:i16,
+    pixelNumberOfImageTopLeft:i16,
+    sampleRatio:i16,
+    latitudeOfNorth:i16,
+    latitudeOfSouth:i16,
+    longitudeOfWest:i16,
+    longitudeOfEast:i16,
+    centerLatitudeOfProjection:i16,
+    centerLongitudeOfProjection:i16,
+    standardLatitude1:i16,
+    standardLatitude2:i16,
+    horizontalResolution:i16,
+    verticalResolution:i16,
+    overlapFlagGeoGrid:i16,
+    overlapValueGeoGrid:i16,
+    dataLengthOfColorTable:i16,
+    dataLengthOfCalibration:i16,
+    dataLengthOfGeolocation:i16,
+    reserved:i16,
+    #[br(if (dataLengthOfColorTable!=0),pad_size_to=256,count=256)]
+    colorR:Option<Vec<u8>>,
+    #[br(if (dataLengthOfColorTable!=0),pad_size_to=256,count=256)]
+    colorG:Option<Vec<u8>>,
+    #[br(if (dataLengthOfColorTable!=0),pad_size_to=256,count=256)]
+    colorB:Option<Vec<u8>>,
+    #[br(if (dataLengthOfCalibration!=0),pad_size_to=256,count=dataLengthOfCalibration/2)]
+    calibration:Option<Vec<u16>>,
+
+}
 
 #[derive(Debug)]
 struct SatReader;
 
 impl MetReader for SatReader {
     fn read<SatReader>(d: &[u8]) -> Result<SatReader, MetError> {
+        let mut reader = Cursor::new(d);
         dbg!(d.len());
+        let header:Product = reader.read_le().unwrap();
+        dbg!(header.productCategory);
+        dbg!(&header.data1.unwrap().len());
+        dbg!(header.header1.unwrap().channel);
+      
 
-        let f_name = &d[0..12];
-        let f_name = String::from_utf8_lossy(&f_name);
-        dbg!(f_name);
-
-        let mut byteSequence = Cursor::new(&d[12..14]);
-        let byteSequence = byteSequence.read_i16::<LittleEndian>()?;
-        dbg!(byteSequence);
-
-        let mut firstClassHeadLength = Cursor::new(&d[14..16]);
-        let firstClassHeadLength = if byteSequence == 0 {
-            firstClassHeadLength.read_i16::<LittleEndian>()?
-        } else {
-            firstClassHeadLength.read_i16::<BigEndian>()?
-        };
-        dbg!(firstClassHeadLength);
-
-        let mut secondClassHeadLength = Cursor::new(&d[16..18]);
-        let secondClassHeadLength = if byteSequence == 0 {
-            secondClassHeadLength.read_i16::<LittleEndian>()?
-        } else {
-            secondClassHeadLength.read_i16::<BigEndian>()?
-        };
-        dbg!(secondClassHeadLength);
-
-        let mut padDataLength = Cursor::new(&d[18..20]);
-        let padDataLength = if byteSequence == 0 {
-            padDataLength.read_i16::<LittleEndian>()?
-        } else {
-            padDataLength.read_i16::<BigEndian>()?
-        };
-        dbg!(padDataLength);
-
-        let mut recordLength = Cursor::new(&d[20..22]);
-        let recordLength = if byteSequence == 0 {
-            recordLength.read_i16::<LittleEndian>()?
-        } else {
-            recordLength.read_i16::<BigEndian>()?
-        };
-        dbg!(recordLength);
-
-        let mut headRecordNumber = Cursor::new(&d[22..24]);
-        let headRecordNumber = if byteSequence == 0 {
-            headRecordNumber.read_i16::<LittleEndian>()?
-        } else {
-            headRecordNumber.read_i16::<BigEndian>()?
-        };
-
-        dbg!(headRecordNumber);
-
-        let mut dataRecordNumber = Cursor::new(&d[24..26]);
-        let dataRecordNumber = if byteSequence == 0 {
-            dataRecordNumber.read_i16::<LittleEndian>()?
-        } else {
-            dataRecordNumber.read_i16::<BigEndian>()?
-        };
-        dbg!(dataRecordNumber);
-
-        let mut productCategory = Cursor::new(&d[26..28]);
-        let productCategory = if byteSequence == 0 {
-            productCategory.read_i16::<LittleEndian>()?
-        } else {
-            productCategory.read_i16::<BigEndian>()?
-        };
-        dbg!(productCategory);
-
-        let mut compressMethod = Cursor::new(&d[28..30]);
-        let compressMethod = if byteSequence == 0 {
-            compressMethod.read_i16::<LittleEndian>()?
-        } else {
-            compressMethod.read_i16::<BigEndian>()?
-        };
-        dbg!(compressMethod);
-
-        let formatString = &d[30..38];
-        let formatString = String::from_utf8_lossy(&formatString);
-        dbg!(formatString);
-
-        let mut qualityFlag = Cursor::new(&d[38..40]);
-        let qualityFlag = if byteSequence == 0 {
-            qualityFlag.read_i16::<LittleEndian>()?
-        } else {
-            qualityFlag.read_i16::<BigEndian>()?
-        };
-        dbg!(qualityFlag);
-
-        let satelliteName = &d[40..48];
-        let satelliteName = String::from_utf8_lossy(&satelliteName);
-        dbg!(satelliteName);
-
-        let mut year = Cursor::new(&d[48..50]);
-        let year = if byteSequence == 0 {
-            year.read_i16::<LittleEndian>()?
-        } else {
-            year.read_i16::<BigEndian>()?
-        };
-        dbg!(year);
-
-        let mut month = Cursor::new(&d[50..52]);
-        let month = if byteSequence == 0 {
-            month.read_i16::<LittleEndian>()?
-        } else {
-            month.read_i16::<BigEndian>()?
-        };
-        dbg!(month);
-
-        let mut day = Cursor::new(&d[52..54]);
-        let day = if byteSequence == 0 {
-            day.read_i16::<LittleEndian>()?
-        } else {
-            day.read_i16::<BigEndian>()?
-        };
-        dbg!(day);
-
-        let mut hour = Cursor::new(&d[54..56]);
-        let hour = if byteSequence == 0 {
-            hour.read_i16::<LittleEndian>()?
-        } else {
-            hour.read_i16::<BigEndian>()?
-        };
-        dbg!(hour);
-
-        let mut minute = Cursor::new(&d[56..58]);
-        let minute = if byteSequence == 0 {
-            minute.read_i16::<LittleEndian>()?
-        } else {
-            minute.read_i16::<BigEndian>()?
-        };
-        dbg!(minute);
-
-        let mut channel = Cursor::new(&d[58..60]);
-        let channel = if byteSequence == 0 {
-            channel.read_i16::<LittleEndian>()?
-        } else {
-            channel.read_i16::<BigEndian>()?
-        };
-        dbg!(channel);
-
-        let mut flagOfProjection = Cursor::new(&d[60..62]);
-        let flagOfProjection = if byteSequence == 0 {
-            flagOfProjection.read_i16::<LittleEndian>()?
-        } else {
-            flagOfProjection.read_i16::<BigEndian>()?
-        };
-        dbg!(flagOfProjection);
-
-        let mut widthOfImage = Cursor::new(&d[62..64]);
-        let widthOfImage = if byteSequence == 0 {
-            widthOfImage.read_i16::<LittleEndian>()?
-        } else {
-            widthOfImage.read_i16::<BigEndian>()?
-        };
-        dbg!(widthOfImage);
-
-        let mut heightOfImage = Cursor::new(&d[64..66]);
-        let heightOfImage = if byteSequence == 0 {
-            heightOfImage.read_i16::<LittleEndian>()?
-        } else {
-            heightOfImage.read_i16::<BigEndian>()?
-        };
-        dbg!(heightOfImage);
-
-        let mut scanLineNumberOfImageTopLeft = Cursor::new(&d[66..68]);
-        let scanLineNumberOfImageTopLeft = if byteSequence == 0 {
-            scanLineNumberOfImageTopLeft.read_i16::<LittleEndian>()?
-        } else {
-            scanLineNumberOfImageTopLeft.read_i16::<BigEndian>()?
-        };
-        dbg!(scanLineNumberOfImageTopLeft);
-
-        let mut pixelNumberOfImageTopLeft = Cursor::new(&d[68..70]);
-        let pixelNumberOfImageTopLeft = if byteSequence == 0 {
-            pixelNumberOfImageTopLeft.read_i16::<LittleEndian>()?
-        } else {
-            pixelNumberOfImageTopLeft.read_i16::<BigEndian>()?
-        };
-        dbg!(pixelNumberOfImageTopLeft);
-
-        let mut sampleRatio = Cursor::new(&d[70..72]);
-        let sampleRatio = if byteSequence == 0 {
-            sampleRatio.read_i16::<LittleEndian>()?
-        } else {
-            sampleRatio.read_i16::<BigEndian>()?
-        };
-        dbg!(sampleRatio);
-
-        let mut latitudeOfNorth = Cursor::new(&d[72..74]);
-        let latitudeOfNorth = if byteSequence == 0 {
-            latitudeOfNorth.read_i16::<LittleEndian>()?
-        } else {
-            latitudeOfNorth.read_i16::<BigEndian>()?
-        };
-        dbg!(latitudeOfNorth);
-
-        let latitudeOfNorth = latitudeOfNorth as f32 / 100.0;
-        dbg!(latitudeOfNorth);
-
-        let mut latitudeOfSouth = Cursor::new(&d[74..76]);
-        let latitudeOfSouth = if byteSequence == 0 {
-            latitudeOfSouth.read_i16::<LittleEndian>()?
-        } else {
-            latitudeOfSouth.read_i16::<BigEndian>()?
-        };
-        dbg!(latitudeOfSouth);
-
-        let latitudeOfSouth = latitudeOfSouth as f32 / 100.0;
-        dbg!(latitudeOfSouth);
-
-        let mut longitudeOfWest = Cursor::new(&d[76..78]);
-        let longitudeOfWest = if byteSequence == 0 {
-            longitudeOfWest.read_i16::<LittleEndian>()?
-        } else {
-            longitudeOfWest.read_i16::<BigEndian>()?
-        };
-        dbg!(longitudeOfWest);
-
-        let longitudeOfWest = longitudeOfWest as f32 / 100.0;
-        dbg!(longitudeOfWest);
-
-        let mut longitudeOfEast = Cursor::new(&d[78..80]);
-        let longitudeOfEast = if byteSequence == 0 {
-            longitudeOfEast.read_i16::<LittleEndian>()?
-        } else {
-            longitudeOfEast.read_i16::<BigEndian>()?
-        };
-        dbg!(longitudeOfEast);
-
-        let longitudeOfEast = longitudeOfEast as f32 / 100.0;
-        dbg!(longitudeOfEast);
-
-        let mut centerLatitudeOfProjection = Cursor::new(&d[80..82]);
-        let centerLatitudeOfProjection = if byteSequence == 0 {
-            centerLatitudeOfProjection.read_i16::<LittleEndian>()?
-        } else {
-            centerLatitudeOfProjection.read_i16::<BigEndian>()?
-        };
-        dbg!(centerLatitudeOfProjection);
-
-        let mut centerLongitudeOfProjection = Cursor::new(&d[82..84]);
-        let centerLongitudeOfProjection = if byteSequence == 0 {
-            centerLongitudeOfProjection.read_i16::<LittleEndian>()?
-        } else {
-            centerLongitudeOfProjection.read_i16::<BigEndian>()?
-        };
-        dbg!(centerLongitudeOfProjection);
-
-        let mut standardLatitude1 = Cursor::new(&d[84..86]);
-        let standardLatitude1 = if byteSequence == 0 {
-            standardLatitude1.read_i16::<LittleEndian>()?
-        } else {
-            standardLatitude1.read_i16::<BigEndian>()?
-        };
-        dbg!(standardLatitude1);
-
-        let mut standardLatitude2 = Cursor::new(&d[86..88]);
-        let standardLatitude2 = if byteSequence == 0 {
-            standardLatitude2.read_i16::<LittleEndian>()?
-        } else {
-            standardLatitude2.read_i16::<BigEndian>()?
-        };
-        dbg!(standardLatitude2);
-
-        let mut horizontalResolution = Cursor::new(&d[88..90]);
-        let horizontalResolution = if byteSequence == 0 {
-            horizontalResolution.read_i16::<LittleEndian>()?
-        } else {
-            horizontalResolution.read_i16::<BigEndian>()?
-        };
-        dbg!(horizontalResolution);
-
-        let mut verticalResolution = Cursor::new(&d[90..92]);
-        let verticalResolution = if byteSequence == 0 {
-            verticalResolution.read_i16::<LittleEndian>()?
-        } else {
-            verticalResolution.read_i16::<BigEndian>()?
-        };
-        dbg!(verticalResolution);
-
-        let mut overlapFlagGeoGrid = Cursor::new(&d[92..94]);
-        let overlapFlagGeoGrid = if byteSequence == 0 {
-            overlapFlagGeoGrid.read_i16::<LittleEndian>()?
-        } else {
-            overlapFlagGeoGrid.read_i16::<BigEndian>()?
-        };
-        dbg!(overlapFlagGeoGrid);
-
-        let mut overlapValueGeoGrid = Cursor::new(&d[94..96]);
-        let overlapValueGeoGrid = if byteSequence == 0 {
-            overlapValueGeoGrid.read_i16::<LittleEndian>()?
-        } else {
-            overlapValueGeoGrid.read_i16::<BigEndian>()?
-        };
-        dbg!(overlapValueGeoGrid);
-
-        let mut dataLengthOfColorTable = Cursor::new(&d[96..98]);
-        let dataLengthOfColorTable = if byteSequence == 0 {
-            dataLengthOfColorTable.read_i16::<LittleEndian>()?
-        } else {
-            dataLengthOfColorTable.read_i16::<BigEndian>()?
-        };
-        dbg!(dataLengthOfColorTable);
-
-        let mut dataLengthOfCalibration = Cursor::new(&d[98..100]);
-        let dataLengthOfCalibration = if byteSequence == 0 {
-            dataLengthOfCalibration.read_i16::<LittleEndian>()?
-        } else {
-            dataLengthOfCalibration.read_i16::<BigEndian>()?
-        };
-        dbg!(dataLengthOfCalibration);
-
-        let mut dataLengthOfGeolocation = Cursor::new(&d[100..102]);
-        let dataLengthOfGeolocation = if byteSequence == 0 {
-            dataLengthOfGeolocation.read_i16::<LittleEndian>()?
-        } else {
-            dataLengthOfGeolocation.read_i16::<BigEndian>()?
-        };
-        dbg!(dataLengthOfGeolocation);
-
-        let mut reserved = Cursor::new(&d[102..104]);
-        let reserved = if byteSequence == 0 {
-            reserved.read_i16::<LittleEndian>()?
-        } else {
-            reserved.read_i16::<BigEndian>()?
-        };
-        dbg!(reserved);
-
-        let mut ind = 104;
-        let head_rest_len = recordLength * headRecordNumber - ind;
-        dbg!(head_rest_len);
-
-        ind += head_rest_len;
-        dbg!(ind);
-
-        let data_len = dataRecordNumber as i32 * recordLength as i32;
-        dbg!(data_len);
-
-        let value = &d[ind as usize..(ind as i32 + data_len) as usize];
-        dbg!(value.len());
 
         Err(MetError::ReadSatError("demo".to_string()))
     }
