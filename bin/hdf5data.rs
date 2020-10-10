d15e1e9f965db87d9955c732d9f0c187301756eb
oldtree2008 <csc00000@sina.com>
2020-10-10 17:40:51 +0800

    debug hdf5 attribute

--- bin/hdf5data.rs
+++ bin/hdf5data.rs
@@ -46,129 +46,165 @@ fn point_value(lon: f32, lat: f32, lons: &[i16], lats: &[i16], values: &[i16]) -
 }
 
 pub fn main() {
-    let fname = r##"H:\data\FY2E\FY2E_2018_11_01_00_31.HDF"##;
+    let fname = r##"/mnt/e/data/FY2E/FY2E_2018_11_01_00_31.HDF"##;
+    // let fname = r##"/mnt/e/kjdata/FY2G-0916-17/FY2G-0916-17/FY2G_2020_09_16_00_01.HDF"##;
+    // let fname = r##"/mnt/e/kjdata/FY2G-0916-17/FY2G-0916-17/FY2G_CBT_2020_09_16_00_01.HDF5"##;
+    // let fname = r##"/mnt/e/kjdata/FY2G-0916-17/FY2G-0916-17/FY2G_CHE_2020_09_16_20_00.HDF5"##;
+    // let fname = r##"/mnt/e/kjdata/FY2G-0916-17/FY2G-0916-17/FY2G_CLC_2020_09_16_20_00.HDF5"##;
+    // let fname = r##"/mnt/e/kjdata/FY2G-0916-17/FY2G-0916-17/FY2G_SST_2020_09_17_07_15.HDF5"##;
+    // let fname = r##"/mnt/e/kjdata/监测预警产品/监测预警产品/飞机颠簸/FY2G_DB202006121201.hdf"##;
+    // let fname = r##"/mnt/e/kjdata/监测预警产品/监测预警产品/飞机颠簸/FY2G_DB202009020000.hdf"##;
+    // let fname = r##"/mnt/e/kjdata/监测预警产品/监测预警产品/飞机积冰/FY2G_JB202009012100.hdf"##;
+    // let fname = r##"/mnt/e/kjdata/监测预警产品/监测预警产品/飞机积冰/FY2G_JB202009040501.hdf"##;
+    // let fname = r##"/mnt/e/kjdata/监测预警产品/监测预警产品/雷暴/FY2G_LB202006121201.HDF"##;
+    // let fname = r##"/mnt/e/kjdata/0930to东华/2.短期预报模式输出样例及格式说明/2020080100/2020080100/domain1/DQYBD1CLT2020080100.dat"##;
     // let p = Path::new("palette/V-01_x.xml");
-    let p = Path::new("palette/I-01.xml");
-    let pal = Palette::new_with_file(&p).unwrap();
+    // let p = Path::new("palette/I-01.xml");
+    // let pal = Palette::new_with_file(&p).unwrap();
     let reader = Hdf5Reader::new(fname);
-    let attrs = reader.as_ref().unwrap().member_names();
+    let reader = reader.as_ref().unwrap();
+    let vars = reader.member_names();
+    dbg!(vars);
+
+    let attrs = reader.attribute_names();
+    let attr = reader.attribute("Column").unwrap();
+    let attr = reader.attribute("Default").unwrap();
+    let attr = reader.attribute("End_Date").unwrap();
+    let shape = attr.shape();
+    // let attr = attr.read_1d::<u16>().unwrap();
+    let attr = attr.read_scalar::<hdf5::types::FixedAscii<[u8;16]>>().unwrap();
+    // let attr = attr.read_scalar::<hdf5::types::VarLenAscii>().unwrap();
     dbg!(attrs);
-    let dataset = reader.as_ref().unwrap().dataset("Latitude");
-    let data = dataset.as_ref();
-    let lat = data.unwrap().read_2d::<i16>().unwrap();
-    let lat_iter = lat.iter();
+    dbg!(shape);
+    dbg!(attr);
 
-    let dataset = reader.as_ref().unwrap().dataset("Longitude");
-    let data = dataset.as_ref();
-    let lon = data.unwrap().read_2d::<i16>().unwrap();
-    let lon_iter = lon.iter();
+    let dataset = reader.dataset("ChannelIR1").unwrap();
+    dbg!(dataset.ndim());
+    // let data = dataset.as_ref();
+    // let data = dataset.read_2d::<i16>().unwrap();
+    // let data = dataset.read_scalar::<hdf5::types::FixedAscii<[u8;16]>>().unwrap();
+    // let data = data.unwrap().read_scalar::<hdf5::types::VarLenAscii>().unwrap();
+    // let data = dataset.read_1d::<i16>().unwrap();
+    // dbg!(data);
+    // let lat_iter = lat.iter();
 
-    let dataset = reader.as_ref().unwrap().dataset("ChannelIR1");
-    let data = dataset.as_ref();
-    let ir1 = data.unwrap().read_2d::<i16>().unwrap();
-    let ir1_iter = ir1.iter();
 
-    dbg!(ir1.len());
-    dbg!(lat_iter.len());
-    dbg!(lon_iter.len());
+    // let dataset = reader.as_ref().unwrap().dataset("Latitude");
+    // let data = dataset.as_ref();
+    // let lat = data.unwrap().read_2d::<i16>().unwrap();
+    // let lat_iter = lat.iter();
 
-    let iter = lat_iter.zip(lon_iter).zip(ir1_iter);
-    let mut lat_vec = Vec::new();
-    let mut lon_vec = Vec::new();
-    // let mut ir1_vec = Vec::new();
-    let mut kdtree = KdTree::new(2);
-    for (latlon, ir1) in iter {
-        let lat = latlon.0;
-        let lon = latlon.1;
-        if *lat != 32765 && *lon != 32765 {
-            lat_vec.push(*lat);
-            lon_vec.push(*lon);
-            // ir1_vec.push(*ir1);
-            kdtree
-                .add(
-                    [*lon as f32 / 100.0, *lat as f32 / 100.0],
-                    *ir1 as f32 / 100.0,
-                )
-                .unwrap();
-            // println!("{}  {}  {}",*lon as f32/100.0, *lat as f32/100.0,*ir1 as f32/100.0);
-        }
-    }
+    // let dataset = reader.as_ref().unwrap().dataset("Longitude");
+    // let data = dataset.as_ref();
+    // let lon = data.unwrap().read_2d::<i16>().unwrap();
+    // let lon_iter = lon.iter();
 
-    println!("kdtree created");
-    let ret = kdtree
-        .nearest(&[113.0, 0.0], 4, &squared_euclidean)
-        .unwrap();
-    let mut nom = 0.0;
-    let mut denom = 0.0;
-    for (dist, value) in ret.iter() {
-        nom += *value / dist;
-        denom += 1.0 / dist;
-    }
-    let v = nom / denom;
+    // let dataset = reader.as_ref().unwrap().dataset("ChannelIR1");
+    // let data = dataset.as_ref();
+    // let ir1 = data.unwrap().read_2d::<i16>().unwrap();
+    // let ir1_iter = ir1.iter();
 
-    println!(" {} ret {:?} ", v, ret);
+    // dbg!(ir1.len());
+    // dbg!(lat_iter.len());
+    // dbg!(lon_iter.len());
 
-    let lat_min = lat_vec.iter().min().unwrap();
-    let lat_max = lat_vec.iter().max().unwrap();
-    let lat_min = *lat_min as f32 / 100.0;
-    let lat_max = *lat_max as f32 / 100.0;
+    // let iter = lat_iter.zip(lon_iter).zip(ir1_iter);
+    // let mut lat_vec = Vec::new();
+    // let mut lon_vec = Vec::new();
+    // // let mut ir1_vec = Vec::new();
+    // let mut kdtree = KdTree::new(2);
+    // for (latlon, ir1) in iter {
+    //     let lat = latlon.0;
+    //     let lon = latlon.1;
+    //     if *lat != 32765 && *lon != 32765 {
+    //         lat_vec.push(*lat);
+    //         lon_vec.push(*lon);
+    //         // ir1_vec.push(*ir1);
+    //         kdtree
+    //             .add(
+    //                 [*lon as f32 / 100.0, *lat as f32 / 100.0],
+    //                 *ir1 as f32 / 100.0,
+    //             )
+    //             .unwrap();
+    //         // println!("{}  {}  {}",*lon as f32/100.0, *lat as f32/100.0,*ir1 as f32/100.0);
+    //     }
+    // }
 
-    let lat_min = 0.0;
-    let lat_max = 60.0;
+    // println!("kdtree created");
+    // let ret = kdtree
+    //     .nearest(&[113.0, 0.0], 4, &squared_euclidean)
+    //     .unwrap();
+    // let mut nom = 0.0;
+    // let mut denom = 0.0;
+    // for (dist, value) in ret.iter() {
+    //     nom += *value / dist;
+    //     denom += 1.0 / dist;
+    // }
+    // let v = nom / denom;
 
-    let step = 0.05;
-    let rows = ((lat_max - lat_min) / step) as usize + 1;
+    // println!(" {} ret {:?} ", v, ret);
 
-    let lon_min = lon_vec.iter().min().unwrap();
-    let lon_max = lon_vec.iter().max().unwrap();
-    let lon_min = *lon_min as f32 / 100.0;
-    let lon_max = *lon_max as f32 / 100.0;
-    let lon_min = 70.0;
-    let lon_max = 140.0;
+    // let lat_min = lat_vec.iter().min().unwrap();
+    // let lat_max = lat_vec.iter().max().unwrap();
+    // let lat_min = *lat_min as f32 / 100.0;
+    // let lat_max = *lat_max as f32 / 100.0;
 
-    let cols = ((lon_max - lon_min) / step) as usize + 1;
+    // let lat_min = 0.0;
+    // let lat_max = 60.0;
 
-    println!("{:?}  {:?}", lat_min, lat_max);
-    println!("{:?}  {:?}", lon_min, lon_max);
-    println!("{:?}  {:?}", rows, cols);
+    // let step = 0.05;
+    // let rows = ((lat_max - lat_min) / step) as usize + 1;
 
-    let mut grid_lats = Vec::with_capacity(rows);
-    for i in 0..rows {
-        grid_lats.push(lat_max - i as f32 * step);
-    }
+    // let lon_min = lon_vec.iter().min().unwrap();
+    // let lon_max = lon_vec.iter().max().unwrap();
+    // let lon_min = *lon_min as f32 / 100.0;
+    // let lon_max = *lon_max as f32 / 100.0;
+    // let lon_min = 70.0;
+    // let lon_max = 140.0;
 
-    let mut grid_lons = Vec::with_capacity(cols);
-    for i in 0..cols {
-        grid_lons.push(lon_min + i as f32 * step);
-    }
-    let num = rows * cols;
-    let mut grid_value = vec![0.0; num];
-    grid_value.par_iter_mut().enumerate().for_each(|(i, d)| {
-        let ii = i / cols;
-        let jj = i % cols;
-        // let v = point_value(grid_lons[jj], grid_lats[ii], &lon_vec, &lat_vec, &ir1_vec);
-        let lon = grid_lons[jj];
-        let lat = grid_lats[ii];
-        let ret = kdtree.nearest(&[lon, lat], 4, &squared_euclidean).unwrap();
-        let mut nom = 0.0;
-        let mut denom = 0.0;
-        for (dist, value) in ret.iter() {
-            nom += *value / dist;
-            denom += 1.0 / dist;
-        }
-        let v = nom / denom;
-        // println!(" {} ret {:?} ",v, ret);
-        *d = v;
-    });
+    // let cols = ((lon_max - lon_min) / step) as usize + 1;
 
-    let mut imgbuf = ImageBuffer::new(cols as u32, rows as u32);
-    // // // // let dd = &data/100;
+    // println!("{:?}  {:?}", lat_min, lat_max);
+    // println!("{:?}  {:?}", lon_min, lon_max);
+    // println!("{:?}  {:?}", rows, cols);
 
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let index = y * cols as u32 + x;
-        let v = grid_value[index as usize];
-        let c = pal.get_color(v as f64).unwrap();
-        *pixel = image::Rgba([c.r, c.g, c.b, c.a]);
-    }
-    imgbuf.save("IR1_1.png").unwrap();
+    // let mut grid_lats = Vec::with_capacity(rows);
+    // for i in 0..rows {
+    //     grid_lats.push(lat_max - i as f32 * step);
+    // }
+
+    // let mut grid_lons = Vec::with_capacity(cols);
+    // for i in 0..cols {
+    //     grid_lons.push(lon_min + i as f32 * step);
+    // }
+    // let num = rows * cols;
+    // let mut grid_value = vec![0.0; num];
+    // grid_value.par_iter_mut().enumerate().for_each(|(i, d)| {
+    //     let ii = i / cols;
+    //     let jj = i % cols;
+    //     // let v = point_value(grid_lons[jj], grid_lats[ii], &lon_vec, &lat_vec, &ir1_vec);
+    //     let lon = grid_lons[jj];
+    //     let lat = grid_lats[ii];
+    //     let ret = kdtree.nearest(&[lon, lat], 4, &squared_euclidean).unwrap();
+    //     let mut nom = 0.0;
+    //     let mut denom = 0.0;
+    //     for (dist, value) in ret.iter() {
+    //         nom += *value / dist;
+    //         denom += 1.0 / dist;
+    //     }
+    //     let v = nom / denom;
+    //     // println!(" {} ret {:?} ",v, ret);
+    //     *d = v;
+    // });
+
+    // let mut imgbuf = ImageBuffer::new(cols as u32, rows as u32);
+    // // // // // let dd = &data/100;
+
+    // for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+    //     let index = y * cols as u32 + x;
+    //     let v = grid_value[index as usize];
+    //     let c = pal.get_color(v as f64).unwrap();
+    //     *pixel = image::Rgba([c.r, c.g, c.b, c.a]);
+    // }
+    // imgbuf.save("IR1_1.png").unwrap();
 }
--- src/reader/hdf5_reader.rs
+++ src/reader/hdf5_reader.rs
@@ -1,6 +1,6 @@
 use crate::error::MetError;
 use hdf5;
-use hdf5::Dataset;
+use hdf5::{Attribute,Dataset};
 
 #[derive(Debug)]
 pub struct Hdf5Reader(Option<hdf5::File>);
@@ -12,9 +12,20 @@ impl Hdf5Reader {
     }
 
     pub fn member_names(&self) -> hdf5::Result<Vec<String>> {
-        let f = &self.0.as_ref().unwrap();
-        f.member_names()
+        let f = &self.0.as_ref().unwrap();        
+        let ms = f.member_names();
+        ms
     }
+    pub fn attribute_names(&self) -> hdf5::Result<Vec<String>> {
+        let f = &self.0.as_ref().unwrap();        
+        let ms = f.attribute_names();
+        ms
+    }
+    pub fn attribute(&self, name: &str) -> hdf5::Result<Attribute> {
+        let f = &self.0.as_ref().unwrap();
+        f.attribute(name)
+    }
+
     pub fn dataset(&self, name: &str) -> hdf5::Result<Dataset> {
         let f = &self.0.as_ref().unwrap();
         f.dataset(name)
@@ -24,4 +35,5 @@ impl Hdf5Reader {
         let d = f.dataset(name).unwrap();
         d.shape()
     }
+
 }
